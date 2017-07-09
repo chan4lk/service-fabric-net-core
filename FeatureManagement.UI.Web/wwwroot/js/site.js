08eb67987d65fa4c3edb987dc4a6d3627c92477c
Chandima Ranaaweera <chan4lk@gmail.com>
2017-07-09 18:40:43 +0530

    cors enabled

--- FeatureManagement.UI.Web/wwwroot/js/site.js
+++ FeatureManagement.UI.Web/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 ﻿// Write your Javascript code.
+$(document).ready(function () {
+    var submit = document.getElementById('add-btn');
+    submit.addEventListener('click', function (e) {
+        $.ajax({
+            url: 'http://localhost:8290/api/values',
+            method: 'GET',
+            accepts: 'application/json',
+            success: function (data) {
+                alert(data);
+            },
+            error: function (err) {
+                if (err) {
+                    alert(err);
+                }
+            }
+        });        
+    });
+});
